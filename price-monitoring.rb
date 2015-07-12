9a6261dbd5734719db4618e5b591a5e417713152
Alessandro Zucca <alessandro.zucca@gmail.com>
2015-07-12 19:49:32 +0100

    adding little script that downloads a page finds a target and logs its digit content into a file

---
+++ price-monitoring.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'hpricot'
+
+puts "Writing in directory: " + File.dirname(File.realpath(__FILE__))
+
+def handleItem(itemName, url, locator) 
+	time = Time.new
+	doc = Hpricot(open(url))
+	price_content = doc.search(locator).inner_html
+	puts "Price found: " + price_content[/\d+/]
+	File.open(File.dirname(File.realpath(__FILE__)) + '/' + itemName + '.txt', 'a') { |file| file.write(time.inspect + " price: " + price_content[/\d+/] + "\n") }
+end
